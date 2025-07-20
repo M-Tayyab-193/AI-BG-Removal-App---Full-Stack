3050a811444ee8e452384ddd441a2b886b807fb8
Muhammad Tayyab Rizwan <tayyabprince193@gmail.com>
2025-07-20 23:30:29 +0500

    changes for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,4 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import { connectDB } from './config/mongodb.js';
+import app from "./app.js";
+import serverless from "serverless-http";
 
-// App Config
-const app = express();
-await connectDB();
-
-// Middlewares
-app.use(express.json());
-app.use(cors());
-
-// Routes
-app.get('/', (req, res) => (
-    res.send('API is working!')
-))
-
-if (process.env.NODE_ENV !== "production") {
-    const PORT = 5000 | process.env.PORT;
-    app.listen(PORT, () => (
-        console.log("Server is running on port:", PORT)
-    ))
-}
-
+export const handler = serverless(app);
